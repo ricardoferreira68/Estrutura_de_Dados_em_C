19810b8aa8383fc49e4e1985a913def98bdfff82
José Ricardo da Silva Ferreira <46268118+ricardoferreira68@users.noreply.github.com>
2021-05-18 14:29:13 -0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ricardo/EstruturaDeDadosEmC3001
-# Build directory: /home/ricardo/EstruturaDeDadosEmC3001/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
