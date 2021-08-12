c095e48f44d4e2fb31fc7502c2e3c8434c32c03e
Ricardo Ferreira <jrsf68@gmail.com>
2021-08-12 20:56:43 -0300

    final
    finalização

---
+++ meu_trabalho.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ncurses.h>
+using namespace std;
+int main(void)
+{
+    initscr();
+    cbreak();
+    noecho();
+    scrollok(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
+    cout << "Início" << endl;
+    while (1)
+    {
+        if (getch() == 'g')
+        {
+            cout << "You pressed G\n" << endl;
+        }
+        //napms(500);
+    }
+    return 0;
+}
